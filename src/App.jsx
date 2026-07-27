282b1cb1c1c57368be3c66ad397ffad54da40990
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-27 12:30:05 +0300

    Add product search and sort functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import CategoryForm from "./components/Category";
 import NavBar from "./components/NavBar";
 import ProductsForm from "./components/Products";
 import ProductList from "./components/ProductList";
+import Filter from "./components/Filter";
 
 function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const [sort, setSort] = useState("latest");
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(() => {}, [products, sort, searchValue]);
+
+  const sortHandler = (e) => {
+    setSort(e.target.value);
+    let sortedProducts = [...products];
+    sortedProducts.sort((a, b) => {
+      if (e.target.value === "latest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? -1 : 1;
+      } else if (e.target.value === "earliest") {
+        return new Date(a.createdAt) > new Date(b.createdAt) ? 1 : -1;
+      }
+    });
+    setFilteredProducts(sortedProducts);
+  };
+
+  const searchHandler = (e) => {
+    setSearchValue(e.target.value);
+    const value = e.target.value.trim().toLowerCase();
+    const filteredProducts = products.filter((p) =>
+      p.title.toLowerCase().includes(value),
+    );
+    setFilteredProducts(filteredProducts);
+  };
+
   return (
     <div className="App">
       <div className="bg-slate-800 min-h-screen">
@@ -15,8 +44,14 @@ function App() {
         <div className="container max-w-screen-sm mx-auto p-4">
           <CategoryForm setCategories={setCategories} />
           <ProductsForm categories={categories} setProducts={setProducts} />
+          <Filter
+            sort={sort}
+            searchValue={searchValue}
+            onSort={sortHandler}
+            onSearch={searchHandler}
+          />
           <ProductList
-            products={products}
+            products={filteredProducts}
             categories={categories}
             setProducts={setProducts}
           />
---
+++ src/components/Filter.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const Filter = ({ onSort, onSearch, sort, searchValue }) => {
+  return (
+    <div>
+      <div className="flex items-center justify-between mb-6">
+        <label htmlFor="search-input" className="text-slate-500 text-lg">
+          search
+        </label>
+        <input
+          type="text"
+          name="search-input"
+          id="search-input"
+          className="bg-transparent rounded-xl border border-slate-500 text-slate-400"
+          value={searchValue}
+          onChange={onSearch}
+        />
+      </div>
+      <div className="flex items-center justify-between mb-6">
+        <label htmlFor="sort-products" className="text-slate-500 text-lg">
+          sort
+        </label>
+        <select
+          name="sort-products"
+          id="sort-products"
+          className="bg-transparent text-slate-400 rounded-xl"
+          value={sort}
+          onChange={onSort}
+        >
+          <option className="bg-slate-500 text-slate-300" value="">
+            select a category
+          </option>
+          <option className="bg-slate-500 text-slate-300" value="Latest">
+            newest
+          </option>
+          <option className="bg-slate-500 text-slate-300" value="Earliest">
+            oldest
+          </option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default Filter;
