81f2407f6527c1a921385fbef95ab808e83f62bf
Yasmin Ebrahimi <yasminebrahimi54@gmail.com>
2026-07-27 20:13:32 +0300

    Update NavBar title capitalization and disable badge

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,14 +2,14 @@ const NavBar = () => {
   return (
     <div className="h-12 flex items-center justify-center gap-x-4 bg-slate-700 mb-6">
       <h1 className="md:text-xl text-sm font-bold text-slate-300">
-        Inventory App using tailwind and react
+        Inventory App using React and Tailwind
       </h1>
-      <span
+      {/* <span
         className="flex items-center justify-center w-7 h-7 rounded-full
           bg-slate-500 border-2 border-slate-300 font-bold text-slate-300"
       >
         7
-      </span>
+      </span> */}
     </div>
   );
 };
